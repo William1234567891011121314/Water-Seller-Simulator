cffc188cda9d536e7cb96b84652b45c62b6e9458
William <131017149+William1234567891011121314@users.noreply.github.com>
2023-10-11 23:00:49 -0300

    Delete assets directory

--- assets/Javascript/celular.js
+++
@@ -1,8 +0,0 @@
-function smartphone() {
-    body.addEventListener('click', ev => {
-        if(!ev.target.closest("#nav") && !ev.target.closest("button")){
-            moneyclick(ev);
-        }
-    });
-    main.removeChild(main.querySelector("#umafr"));
-}
