ac31f31b59a010203e777173068e2e837cdb60fd
William1234567891011121314 <williamboeira429@gmail.com>
2023-10-14 20:01:15 -0300

    inicio quiz

---
+++ assets/Javascript/quiz.js
@@ -0,0 +1,194 @@
+function quiz() {
+    fatherpopup.innerHTML = `
+        <div id="quiz">
+            <div id="navquiz">
+                <div>
+                    <img src="./assets/favicon.ico">
+                    <p>Waterscape navigator</p>
+                </div>
+                <div id="navbut">
+                    <button id="btmin"><img src="./assets/min.png"></button>
+                    <button id="btmax"><img src="./assets/max.png"></button>
+                    <button id="btesc"><img src="./assets/esc.png"></button>
+                </div>
+            </div>
+            <div id="site">
+                <div>
+                    <input id="url" type="text" value="https://www.booble.com.br/login.html"><button><p>Pesquisar</p></button>
+                </div>
+                <div id="cxdiag">
+                    <div>
+                        <div id="input">
+                            <p>Senha:</p>
+                            <input type="text">
+                            <p id="incorreto">Senha incorreta</p>
+                        </div>
+                        <div id="botoes">
+                            <button id="btforget"><p>Esqueci minha senha</p></button>
+                            <button id="btconfirm"><p>Confirmar</p></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+    fatherpopup.classList.add("visivel");
+    const input = fatherpopup.querySelector("div#input");
+    const txterror = input.querySelector("p#incorreto");
+    const btforget = fatherpopup.querySelector("button#btforget");
+    const btconfirm = fatherpopup.querySelector("button#btconfirm");
+    const navbut = fatherpopup.querySelector("#navbut");
+    const btmin = navbut.querySelector("button#btmin");
+    const btmax = navbut.querySelector("button#btmax");
+    const btesc = navbut.querySelector("button#btesc");
+    input.addEventListener('keydown', ev => {
+        if(ev.key=='Enter'){
+            txterror.classList.add("visivel");
+            erroraudio.play();
+        }
+    })
+    btconfirm.addEventListener('click', () => {
+        txterror.classList.add("visivel");
+        erroraudio.play();
+    })
+    btesc.addEventListener('click', () => {
+        fatherpopup.classList.remove("visivel");
+    })
+    btforget.addEventListener('click', () => {
+        const cxdiag = fatherpopup.querySelector("div#cxdiag");
+        const url = fatherpopup.querySelector("input#url");
+        url.setAttribute("value", "https://www.booble.com.br/login/recovery.html");
+        cxdiag.innerHTML = `
+            <div>
+                <label></label>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+            </div>
+            <div>
+                <label></label>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+            </div>
+            <div>
+                <label></label>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+            </div>
+            <div>
+                <label></label>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+            </div>
+            <div>
+                <label></label>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+            </div>
+            <div>
+                <label></label>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+            </div>
+            <div>
+                <label></label>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+            </div>
+            <div>
+                <label></label>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+            </div>
+            <div>
+                <label></label>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+            </div>
+            <div>
+                <label></label>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+                <input type="radio"></input>
+            </div>
+        `
+    })
+}
